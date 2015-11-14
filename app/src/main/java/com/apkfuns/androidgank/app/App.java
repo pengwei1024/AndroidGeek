3719cfae75c735aa1cbbe995f1d0ac1015deef01
NingSo <ningdev@163.com>
2015-11-14 21:46:07 +0800

    delete google-play-services.jar

--- app/src/main/java/com/apkfuns/androidgank/app/App.java
+++ app/src/main/java/com/apkfuns/androidgank/app/App.java
@@ -1,19 +1,11 @@
 package com.apkfuns.androidgank.app;
 
 import android.app.Application;
-import android.content.Context;
-import android.support.multidex.MultiDex;
 
 /**
  * Created by pengwei08 on 15/11/14.
  */
 public class App extends Application {
-
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        MultiDex.install(this);
-    }
 
     @Override
     public void onCreate() {
